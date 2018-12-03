6fb28c0746d5ae0c218fa03c0e713cc0ea8ef340
A00428662 <109015097@qq.com>
2018-12-03 16:23:49 -0400

    Add room pic url to ROOM

--- SQL Create Queries.sql
+++ SQL Create Queries.sql
@@ -54,7 +54,8 @@ CREATE TABLE [dbo].[ROOM](
 	[roomName] [varchar](50) NOT NULL,
 	[roomNum] [int] NOT NULL,
 	[roomType] [varchar](50) NOT NULL,
-	[price(in CAD)] [int] NOT NULL
+	[price(in CAD)] [int] NOT NULL,
+	[roomURL] [varchar](128)
 ) ON [PRIMARY]
 GO
 
@@ -96,69 +97,88 @@ INSERT INTO [dbo].[ROOM]
            ,[roomNum]
            ,[roomType]
            ,[price(in CAD)])
+		   ,[roomURL]
      VALUES
            ('Apple'
            ,101
            ,'Single'
-           ,90),
+           ,90,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(1).jpg?alt=media&token=bbe1944b-7576-4e34-99d3-93939ab7e388')
 		   ('Banana'
            ,102
            ,'Double'
-           ,120),
+           ,120,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(10).jpg?alt=media&token=86c9b6d0-9dbf-4c24-b76b-cd9c5e5d123b'),
 		   ('Cherry'
            ,103
            ,'Triple'
-           ,130),
+           ,130,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(11).jpg?alt=media&token=c5e2818f-ce04-4a52-934a-bcf74a27fe08'),
 		   ('Damson'
            ,104
            ,'Quad'
-           ,150),
+           ,150,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(12).jpg?alt=media&token=f4024417-274b-4881-9718-7b2735bc0258'),
 		   ('Elderberry'
            ,105
            ,'Queen'
-           ,140),
+           ,140,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(13).jpg?alt=media&token=76756117-aec1-43b4-b464-126c5df6917a'),
 		   ('Fig'
            ,201
            ,'King'
-           ,160),
+           ,160,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(14).jpg?alt=media&token=322c31a0-5686-4125-a848-0d458ed43777'),
 		   ('Grape'
            ,202
            ,'Twin'
-           ,110),
+           ,110,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(15).jpg?alt=media&token=d0e49421-db33-4034-a200-dd769862853c'),
 		   ('Hackberry'
            ,203
            ,'Twin'
-           ,110),
+           ,110,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(17).jpg?alt=media&token=4db08bd3-f0df-417e-b30f-12c75479bd48'),
 		   ('Indian Prune'
            ,204
            ,'Single'
-           ,90),
+           ,90,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(18).jpg?alt=media&token=056e5af6-2f84-49b7-86a3-6657d61745fe'),
 		   ('Jambolan'
            ,205
            ,'Single'
-           ,90),
+           ,90,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(19).jpg?alt=media&token=6af78fc0-1726-4500-8c80-3a3b2f81c26e'),
 		   ('Kiwi'
            ,301
            ,'Master Suite'
-           ,290),
+           ,290,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(2).jpg?alt=media&token=124c59cf-5f2d-413c-b94f-3a0019d07b41'),
 		   ('Lime'
            ,302
            ,'Queen'
-           ,140),
+           ,140,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(20).jpg?alt=media&token=e9c43ec7-ee76-43ad-99fe-e8b4db5dae90'),
 		   ('Mango'
            ,303
            ,'King'
-           ,160),
+           ,160,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(21).jpg?alt=media&token=eb0dfc06-a73d-499e-b09d-f6520ba404ce'),
 		   ('Nectarine'
            ,304
            ,'Double'
-           ,125),
+           ,125,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(22).jpg?alt=media&token=2f07bf8f-13d1-4355-b3db-37e28d09845d'),
 		   ('Orange'
            ,305
            ,'Triple'
-           ,135),
+           ,135,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(23).jpg?alt=media&token=ca08af31-fe02-4d2d-a04a-630fa3d80434'),
 		   ('Papaya'
            ,401
            ,'Double'
-           ,100)
+           ,100,
+		   'https://firebasestorage.googleapis.com/v0/b/mcda-hotel.appspot.com/o/hotel-rooms%2FHotel-Room%20(24).jpg?alt=media&token=71e24001-e87f-4d0f-baf2-daeb425bf204')
 GO
+
+
