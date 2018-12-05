bc9e76b04b96f4af8db601709c89bd7d9a4c34fa
A00428662 <109015097@qq.com>
2018-12-05 00:02:53 -0400

    Booking room changes

--- HotelReservationSystem/HotelReservationSystem/Controllers/BookingController.cs
+++ HotelReservationSystem/HotelReservationSystem/Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelReservationSystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,15 +22,35 @@ namespace HotelReservationSystem.Controllers
             return View(booking);
         }
 
-        public ActionResult BookNow(BOOKING bookingDetails)
+        public ActionResult SearchRooms(BOOKING bookingDetails)
         {
-            if (Session["userId"]!=null && Session["userName"]!=null){
+            Session["fromDate"] = bookingDetails.fromDate;
+            Session["toDate"] = bookingDetails.toDate;
+            Session["adults"] = bookingDetails.adults;
+            Session["children"] = bookingDetails.children;
 
-            }
-
-                return View();
+            BookingModel model = new BookingModel();
+            string query = "SELECT * FROM ROOM WHERE roomId NOT IN (SELECT FK_RID FROM BOOKING WHERE fromDate =@fromDate AND toDate=@toDate)";
+            IEnumerable<ROOM> roomList = model.Database.SqlQuery<ROOM>(query, new SqlParameter("@fromDate", bookingDetails.fromDate), new SqlParameter("@toDate", bookingDetails.toDate));
+            ViewBag.RoomList = roomList.ToList();
+            return View("BookingHome");
         }
 
-        
+        [HttpPost]
+        public ActionResult BookNow(FormCollection form)
+        {
+            Session["roomId"] = form["roomId"];
+            Session["roomPrice"] = form["roomPrice"];
+            if (Session["userId"] == null) {
+                return RedirectToAction("Login", "User");
+            }
+            //string toDate = Session["fromDate"].ToString();
+            //string fromDate = Session["toDate"].ToString();
+            //string adults = Session["adults"].ToString();
+            //string children = Session["children"].ToString();
+            //string userId = Session["userId"].ToString();
+            //string roomId=Session["roomID"].ToString();
+            return RedirectToAction("Paynow", "Payment");
+        }
     }
 }
